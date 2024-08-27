21306f361ef6e26eb5bb3603d46779a199eaf776
shekhar yadav <geminishekhar4012@gmail.com>
2024-08-27 12:30:42 -0500

    6th commit

--- DataTypes/BooleanDT.py
+++ DataTypes/BooleanDT.py
@@ -5,6 +5,9 @@ print("git 1st commit")
 print("git 2nd commit")
 print("git 3rd commit")
 print("git 4th commit")
+<<<<<<< HEAD
 print("git 5th commit")
 print("git 6th commit")
 
+=======
+>>>>>>> parent of f32df81 (5th commit)
